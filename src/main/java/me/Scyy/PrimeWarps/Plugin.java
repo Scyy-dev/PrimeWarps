15de0e19e699de6dbf1d226f959c7a37c0a5afc9
Scyy <bamslizard3201@gmail.com>
2020-12-08 20:41:10 +0800

    Registered all Configs, Registers and Commands in the plugin

--- src/main/java/me/Scyy/PrimeWarps/Plugin.java
+++ src/main/java/me/Scyy/PrimeWarps/Plugin.java
@@ -1,13 +1,68 @@
 package me.Scyy.PrimeWarps;
 
+import me.Scyy.PrimeWarps.Commands.PlayerWarpAdminCommand;
+import me.Scyy.PrimeWarps.Commands.WarpRequestCommand;
+import me.Scyy.PrimeWarps.Config.ConfigFileHandler;
+import me.Scyy.PrimeWarps.Commands.PlayerWarpCommand;
+import me.Scyy.PrimeWarps.Warps.WarpRegister;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Plugin extends JavaPlugin {
 
     private BidRegister bidRegister;
 
+    private WarpRegister warpRegister;
+
+    private ConfigFileHandler CFH;
+
     @Override
     public void onEnable() {
+        this.CFH = new ConfigFileHandler(this);
+
         this.bidRegister = new BidRegister();
+        this.warpRegister = new WarpRegister(CFH.getPlayerWarps().loadWarps(), CFH.getPlayerWarps().loadWarpRequests());
+
+        // Register all the commands
+        PlayerWarpCommand playerWarpCommand = new PlayerWarpCommand(this);
+        WarpRequestCommand warpRequestCommand = new WarpRequestCommand(this);
+        PlayerWarpAdminCommand playerWarpAdminCommand = new PlayerWarpAdminCommand(this);
+        this.getCommand("pwarp").setExecutor(playerWarpCommand);
+        this.getCommand("pwarp").setTabCompleter(playerWarpCommand);
+        this.getCommand("warprequest").setExecutor(warpRequestCommand);
+        this.getCommand("warprequest").setTabCompleter(warpRequestCommand);
+        this.getCommand("pwadmin").setExecutor(playerWarpAdminCommand);
+        this.getCommand("pwadmin").setTabCompleter(playerWarpAdminCommand);
+
+
+
+    }
+
+    public BidRegister getBidRegister() {
+        return bidRegister;
+    }
+
+    public ConfigFileHandler getCFH() {
+        return CFH;
+    }
+
+    public WarpRegister getWarpRegister() {
+        return warpRegister;
+    }
+
+    public void reload(CommandSender sender) {
+        try {
+            CFH.reloadConfigs();
+            sender.sendMessage("Successfully reloaded!");
+        } catch (Exception e) {
+            sender.sendMessage("Error reloading! Check console for logs!");
+            e.printStackTrace();
+        }
+    }
+
+    public void splashText(CommandSender sender) {
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6&lPrime&7&lWarps"));
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6Built by &3_Scyy"));
     }
 }
